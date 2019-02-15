4d2f326737d9bf750a51dca33b176ab0d1cc42f5
Will Smith <will.smith@buildingconnected.com>
2019-02-14 23:31:50 -0800

    Change VIMRUNTIMEDIR to home

--- install_vim.sh
+++ install_vim.sh
@@ -3,16 +3,6 @@
 cd ~
 git clone https://github.com/vim/vim.git
 cd vim
-./configure --with-features=huge \
-	--enable-multibyte \
-	--enable-rubyinterp=yes \
-	--enable-pythoninterp=yes \
-	--with-python-config-dir=/usr/lib/python2.7/config \ # pay attention here check directory correct
-	--enable-python3interp=yes \
-	--with-python3-config-dir=/usr/lib/python3.5/config \
-	--enable-perlinterp=yes \
-	--enable-luainterp=yes \
-	--enable-cscope \
-	--prefix=/usr/local
+./configure --with-features=huge --prefix=$HOME
 
-make VIMRUNTIMEDIR=/usr/local/share/vim/vim81
+make VIMRUNTIMEDIR=$HOME
